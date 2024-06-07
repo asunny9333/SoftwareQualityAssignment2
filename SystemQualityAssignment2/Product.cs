dd8988a4032123f3e1f7feacc3e93df7e18756cd
asunny9333 <asunny9333@conestogac.on.ca>
2024-06-07 13:56:38 -0400

    second commit - added new product class with methods to update stocks

---
+++ SystemQualityAssignment2/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemQualityAssignment2
+{
+    internal class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+    
+
+
+
+        public Product(int productId, string productName, decimal price, int stock)
+        {
+            ProductID = productId;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        public void IncreaseStock(int amount)
+        {
+         Stock += amount;
+         Console.WriteLine("Stock updated");
+        }
+
+        public void DecreaseStock(int amount)
+        {
+            if (Stock >= amount)
+            {
+            Stock -= amount;
+            Console.WriteLine("Stock reduced");
+            }
+        }
+    }
+}
+
